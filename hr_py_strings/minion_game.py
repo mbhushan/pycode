241a19d319d6736c6d80c4ee538781780299994a
manibhushan <manibhushan.cs@gmail.com>
2016-10-19 06:36:34 -0700

    slow minion game version

--- hr_py_strings/minion_game.py
+++ hr_py_strings/minion_game.py
@@ -16,23 +16,34 @@ def main():
     s = input().strip()
     kevin = 0
     stuart = 0
+    kevin_words = set()
+    stuart_words = set()
     vowels = "AEIOU"
     slen = len(s)
     for i in range(slen):
         if vowels.find(s[i]) >= 0:
             L = 1
             while L <= len(s[i:]):
-                kevin = kevin + find_count(s[i:], s[i:i+L])
+                if s[i:i+L] not in kevin_words:
+                    kevin = kevin + find_count(s[i:], s[i:i+L])
+                kevin_words.add(s[i:i+L])
                 L = L + 1
+
         else:
             L = 1
             while L <= len(s[i:]):
-                stuart = stuart + find_count(s[i:], s[i:i+L])
+                if s[i:i+L] not in stuart_words:
+                    stuart = stuart + find_count(s[i:], s[i:i+L])
+                stuart_words.add(s[i:i+L])
                 L = L + 1
     print("kevin: ", kevin)
     print("stuart: ", stuart)
-
-
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif kevin < stuart:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
 
 
 if __name__ == '__main__':
