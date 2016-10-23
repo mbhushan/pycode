011eec49054a59978d6b267f0b702663f1ccd816
manibhushan <manibhushan.cs@gmail.com>
2016-10-23 03:01:30 -0700

    print n

---
+++ hr_python/print_n.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-print
+def main():
+    n = int(input().strip())
+    for i in range(n):
+        print(i+1, end="")
+
+
+if __name__ == '__main__':
+    main()
