3e0cd74ec1d0e0ad7416e6872e72a6ebdf84b3f5
manibhushan <manibhushan.cs@gmail.com>
2016-10-23 03:01:17 -0700

    operators

---
+++ hr_python/operators.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators
+def main():
+    a = int(input().strip())
+    b = int(input().strip())
+    print((a+b))
+    print((a-b))
+    print((a*b))
+
+if __name__ == '__main__':
+    main()
