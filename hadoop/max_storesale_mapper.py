234476988c7af521e31deb57648f3754b499507f
manibhushan <manibhushan.cs@gmail.com>
2016-10-31 05:50:14 -0700

    max storesale mapper

---
+++ hadoop/max_storesale_mapper.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def mapper():
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        if len(data) != 6:
+            continue
+        date, time, store, item, cost, payment = data
+        print("%s\t%s" % (item, cost))
+
+def main():
+    mapper()
+
+if __name__ == '__main__':
+    main()
