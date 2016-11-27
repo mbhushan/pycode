8cfac8cf70c41bcbcf756849b6237e0c6d7e7dce
manibhushan <manibhushan.cs@gmail.com>
2016-11-27 11:25:18 -0800

    combine dataets reducer

---
+++ hadoop/combine_datasets_reducer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Here you will be able to combine the values that come from 2 sources
+# Value that starts with A will be the user data
+# Values that start with B will be forum node data
+
+import sys
+
+
+def reducer():
+    for line in sys.stdin:
+
+# YOUR CODE HERE
